007ee32ff23dff5e519068324aa2cf3f3983701a
William Jeyaraj <william.d.jeyaraj@gmail.com>
2023-01-21 15:43:32 -0700

    Initial project creation

---
+++ src/main/java/com/jwd/tws/TopWordSequenceApplication.java
@@ -0,0 +1,13 @@
+package com.jwd.tws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopWordSequenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopWordSequenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwd/tws/TopWordSequenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwd.tws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopWordSequenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
